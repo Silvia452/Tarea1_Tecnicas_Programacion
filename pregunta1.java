3a3c8a95d9e6a72dc05a3ffcae90537f2e9177a7
Silvia452 <78861903+Silvia452@users.noreply.github.com>
2023-02-02 18:44:36 +0000

    hemos creado las preguntas

---
+++ pregunta1.java
@@ -0,0 +1,19 @@
+// ¿Qué algoritmos usas en tu vida cotidiana? 
+
+// generame el codigo de ejemplo para la pregunta el algoritmop de mi vida cotidiana en java con programación orientada a objetos
+
+
+// ¿Qué algoritmos usas en tu vida cotidiana?
+
+// ¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
+
+
+
+
+
+
+
+
+
+
---
+++ pregunta2.java
@@ -0,0 +1,13 @@
+// ¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
+// codigo de java
+
+
+class pregunta2 {
+    public static void main(String[] args) {
+        System.out.println("¿Qué algoritmos usas en tu vida cotidiana?");
+    }
+}
+
+//
+
